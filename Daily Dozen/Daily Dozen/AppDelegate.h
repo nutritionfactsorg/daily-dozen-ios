fd6c4561abae1c9e3c25605d9a671e857a51b651
Chan Kruse <chan.kruse@qxmd.com>
2015-10-02 12:04:06 -0700

    - creating project from template

---
+++ Daily Dozen/Daily Dozen/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  Daily Dozen
+//
+//  Created by Chan Kruse on 2015-10-02.
+//  Copyright © 2015 NutritionFacts.org. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ Daily Dozen/Daily Dozen/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Daily Dozen
+//
+//  Created by Chan Kruse on 2015-10-02.
+//  Copyright © 2015 NutritionFacts.org. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
