8676e87d73aa3a03fbf3d68dcb6d7892d23c0fb7
Will Webb <will.webb@software.dell.com>
2016-09-01 18:40:18 -0500

    Creates swift project template for 2.0 app

---
+++ DailyDozen/DailyDozen/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DailyDozen
+//
+//  Created by Will Webb on 9/1/16.
+//  Copyright © 2016 NutritionFacts.org. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
