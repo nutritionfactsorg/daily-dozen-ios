8de9c0a73e3071bc59b94a5f8b2970c43eef993d
Will Webb <will.webb@software.dell.com>
2016-09-06 12:41:15 -0500

    Adds a very basic UI to the application (not polished or hooked up to data yet)

---
+++ DailyDozen/DailyDozen/CheckboxButton.swift
@@ -0,0 +1,32 @@
+//
+//  CheckboxButton.swift
+//  DailyDozen
+//
+//  Created by Will Webb on 9/4/16.
+//  Copyright © 2016 NutritionFacts.org. All rights reserved.
+//
+
+import UIKit
+
+class CheckboxButton: UIButton {
+    
+    let checkedImage = UIImage(named: "images/checkmark_filled")
+    let uncheckedImage = UIImage(named: "images/checkmark_unfilled")
+    
+    var checked: Bool = false {
+        didSet {
+            setImage(checked ? checkedImage : uncheckedImage, forState: .Normal)
+        }
+    }
+    
+    override func awakeFromNib() {
+        addTarget(self, action: #selector(CheckboxButton.onClick(_:)), forControlEvents: UIControlEvents.TouchUpInside)
+        checked = false
+    }
+    
+    func onClick(sender: UIButton) {
+        if(sender == self) {
+            checked = !checked
+        }
+    }
+}
--- DailyDozen/DailyDozen/ViewController.swift
+++ DailyDozen/DailyDozen/ViewController.swift
@@ -8,18 +8,62 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
+    
+    let servingNames = ["Beans", "Berries", "Other Fruits", "Cruciferous Vegetables", "Greens", "Other Vegetables", "Flaxseeds", "Nuts", "Spices", "Whole Grains", "Beverages", "Exercise"]
+    let servingImages = ["ic_beans", "ic_berries", "ic_other_fruits", "ic_cruciferous", "ic_greens", "ic_other_veg", "ic_flax", "ic_nuts", "ic_spices", "ic_whole_grains", "ic_beverages", "ic_exercise"]
+    let servingSizes = [3, 1, 3, 1, 2, 2, 1, 1, 1, 3, 5, 1]
 
+    @IBOutlet weak var servingTableView: UITableView!
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    
+        return servingNames.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+    
+        let cell: UITableViewCell = servingTableView.dequeueReusableCellWithIdentifier("ServingCell") as UITableViewCell!
+        let servingImage = cell.contentView.viewWithTag(1) as! UIImageView
+        let servingText = cell.contentView.viewWithTag(2) as! UILabel
+        let servingInfoButton = cell.contentView.viewWithTag(3) as! UIButton
+        let servingCheckboxes = [
+            cell.contentView.viewWithTag(8) as! CheckboxButton,
+            cell.contentView.viewWithTag(7) as! CheckboxButton,
+            cell.contentView.viewWithTag(6) as! CheckboxButton,
+            cell.contentView.viewWithTag(5) as! CheckboxButton,
+            cell.contentView.viewWithTag(4) as! CheckboxButton,
+        ]
+        let servingDateButton = cell.contentView.viewWithTag(9) as! UIButton
+    
+        servingImage.image = UIImage(named: "images/" + servingImages[indexPath.row])
+        servingImage.contentMode = .Center
+        servingText.text = servingNames[indexPath.row]
+        for checkboxIndex in 0...4 {
+            
+            servingCheckboxes[checkboxIndex].hidden = !(checkboxIndex < servingSizes[indexPath.row])
+        }
+        
+        return cell
+    }
+    
     override func viewDidLoad() {
+    
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        servingTableView.dataSource = self
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        
+        self.title = "Daily Dozen"
     }
 
     override func didReceiveMemoryWarning() {
+    
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
