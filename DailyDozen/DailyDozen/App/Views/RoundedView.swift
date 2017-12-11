a4557f0869c489a5d4bd932bf44c709c08ff0b88
Konstantin Khokhlov <sqb@mail.ru>
2017-12-11 15:15:28 +0400

    MODULE: App - Added RoundedView

---
+++ DailyDozen/DailyDozen/App/Views/RoundedView.swift
@@ -0,0 +1,62 @@
+//
+//  RoundedView.swift
+//  DailyDozen
+//
+//  Created by Konstantin Khokhlov on 11.12.2017.
+//  Copyright © 2017 Nutritionfacts.org. All rights reserved.
+//
+
+import UIKit
+
+//@IBDesignable
+class RoundedView: UIView {
+
+    @IBInspectable
+    var cornerRadius: CGFloat = 5 {
+        didSet {
+            layer.cornerRadius = cornerRadius
+        }
+    }
+
+    @IBInspectable
+    var borderWidth: CGFloat = 1 {
+        didSet {
+            layer.borderWidth = borderWidth
+        }
+    }
+
+    @IBInspectable
+    var borderColor: UIColor = UIColor.clear {
+        didSet {
+            layer.borderColor = borderColor.cgColor
+        }
+    }
+
+    @IBInspectable
+    var shadowColor: UIColor = UIColor.clear {
+        didSet {
+            layer.shadowColor = shadowColor.cgColor
+        }
+    }
+
+    @IBInspectable
+    var shadowRadius: CGFloat = 2 {
+        didSet {
+            layer.shadowRadius = 2
+        }
+    }
+
+    @IBInspectable
+    var shadowOffset: CGSize = CGSize.zero {
+        didSet {
+            layer.shadowOffset = shadowOffset
+        }
+    }
+
+    @IBInspectable
+    var shadowOpacity: Float = 1 {
+        didSet {
+            layer.shadowOpacity = shadowOpacity
+        }
+    }
+}
