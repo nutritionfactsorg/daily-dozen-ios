9ce65992ae81e3be5a6f9f35581a91b3278cfc8e
Konstantin Khokhlov <sqb@mail.ru>
2018-01-29 14:14:03 +0400

    Servings: Updated the date button styles

--- DailyDozen/DailyDozen/App/Views/RoundedButton.swift
+++ DailyDozen/DailyDozen/App/Views/RoundedButton.swift
@@ -32,6 +32,34 @@ class RoundedButton: UIButton {
         }
     }
 
+    @IBInspectable
+    var shadowColor: UIColor = UIColor.clear {
+        didSet {
+            layer.shadowColor = shadowColor.cgColor
+        }
+    }
+
+    @IBInspectable
+    var shadowRadius: CGFloat = 2 {
+        didSet {
+            layer.shadowRadius = 2
+        }
+    }
+
+    @IBInspectable
+    var shadowOffset: CGSize = CGSize.zero {
+        didSet {
+            layer.shadowOffset = shadowOffset
+        }
+    }
+
+    @IBInspectable
+    var shadowOpacity: Float = 1 {
+        didSet {
+            layer.shadowOpacity = shadowOpacity
+        }
+    }
+
     override var isEnabled: Bool {
         didSet {
             layer.borderColor = isEnabled ? borderColor.cgColor : borderColor.withAlphaComponent(0.5).cgColor
