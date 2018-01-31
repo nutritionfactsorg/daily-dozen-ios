0f75983e95159e019dfe2579678b45cfa44a650c
Konstantin Khokhlov <sqb@mail.ru>
2018-01-31 14:44:18 +0400

    Menu: Added the cookbook link

--- DailyDozen/DailyDozen/Menu/Controllers/MenuViewController.swift
+++ DailyDozen/DailyDozen/Menu/Controllers/MenuViewController.swift
@@ -12,55 +12,12 @@ import UIKit
 class MenuViewController: UITableViewController {
 
     // MARK: - Nested
-    private struct Links {
-        static let videos = "videos"
-        static let book = "book"
-        static let donate = "donate"
-        static let subscribe = "subscribe"
-        static let source = "open-source"
-    }
-
     private struct Strings {
         static let menu = "Menu"
     }
 
     private struct Keys {
         static let realm = "main.realm"
-    }
-
-    private enum MenuItem: Int {
-
-        case servings, videos, book, donate, subscribe, source, settings, backup, about
-
-        var link: String? {
-            switch self {
-            case .servings, .settings, .backup, .about:
-                return nil
-            case .videos:
-                return Links.videos
-            case .book:
-                return Links.book
-            case .donate:
-                return Links.donate
-            case .subscribe:
-                return Links.subscribe
-            case .source:
-                return Links.source
-            }
-        }
-
-        var controller: UIViewController? {
-            switch self {
-            case .servings:
-                return PagerBuilder.instantiateController()
-            case .about:
-                return AboutBuilder.instantiateController()
-            case .settings:
-                return ReminderBuilder.instantiateController()
-            case .videos, .book, .donate, .subscribe, .source, .backup:
-                return nil
-            }
-        }
     }
 
     // MARK: - UITableViewController
