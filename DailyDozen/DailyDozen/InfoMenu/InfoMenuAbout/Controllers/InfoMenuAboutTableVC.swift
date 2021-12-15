2b8f1baaa62f28039e2a3742c7b3329909552873
marc-medley <marc-medley@users.noreply.github.com>
2021-12-15 01:25:01 -0800

    adds localization to Info About App screen. About App no longer requires ActiveLabel. fixed layout to not truncate (de).

--- DailyDozen/DailyDozen/InfoMenu/InfoMenuAbout/Controllers/InfoMenuAboutTableVC.swift
+++ DailyDozen/DailyDozen/InfoMenu/InfoMenuAbout/Controllers/InfoMenuAboutTableVC.swift
@@ -39,9 +39,35 @@ class InfoMenuAboutTableVC: UITableViewController {
     @IBOutlet private weak var messageLabel: ActiveLabel!
     @IBOutlet private weak var infoLabel: ActiveLabel!
     @IBOutlet private weak var designLabel: ActiveLabel!
-
+    
+    // Localized outlets
+    @IBOutlet weak var infoAppAboutAppName: UILabel!
+    @IBOutlet weak var infoAppAboutCreatedBy: UILabel!
+    @IBOutlet weak var infoAppAboutOssCredits: UILabel!
+    @IBOutlet weak var infoAppAboutOverview: UILabel!    
+    @IBOutlet weak var infoAppAboutVersion: UILabel!
+    @IBOutlet weak var infoAppAboutWelcome: UILabel!
+    
     // MARK: - UITableViewController
     override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
+        navigationController?.navigationBar.barTintColor = ColorManager.style.mainMedium
+        navigationController?.navigationBar.tintColor = UIColor.white
+
+        let barItem = UIBarButtonItem(title: "Done", style: .done, target: nil, action: nil)
+        barItem.tintColor = UIColor.white
+        navigationItem.setLeftBarButton(barItem, animated: false)
+
+        infoAppAboutAppName.text = NSLocalizedString("info_app_about_app_name", comment: "")
+        infoAppAboutCreatedBy.text = NSLocalizedString("info_app_about_created_by", comment: "")
+        infoAppAboutOssCredits.text = NSLocalizedString("info_app_about_oss_credits", comment: "")
+        infoAppAboutOverview.text = NSLocalizedString("info_app_about_overview", comment: "")
+        infoAppAboutVersion.text = NSLocalizedString("info_app_about_version", comment: "")
+        infoAppAboutWelcome.text = NSLocalizedString("info_app_about_welcome", comment: "")
+    }
+
+    func viewDidLoad_WAS() {
         super.viewDidLoad()
         navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
         navigationController?.navigationBar.barTintColor = ColorManager.style.mainMedium
