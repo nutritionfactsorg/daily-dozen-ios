9205a1367a95c36672933cbb01faddb7b7638bb5
marc-medley <marc-medley@users.noreply.github.com>
2022-11-03 19:59:36 -0700

    about screen layout for contributions

--- DailyDozen/DailyDozen/InfoMenu/InfoMenuAbout/Controllers/InfoMenuAboutTableVC.swift
+++ DailyDozen/DailyDozen/InfoMenu/InfoMenuAbout/Controllers/InfoMenuAboutTableVC.swift
@@ -38,8 +38,7 @@ class InfoMenuAboutTableVC: UITableViewController {
     
     // Localized outlets
     @IBOutlet weak var infoAppAboutAppName: UILabel!
-    @IBOutlet weak var infoAppAboutCreatedBy: UILabel!
-    @IBOutlet weak var infoAppAboutOssCredits: UILabel!
+    @IBOutlet weak var infoAppAboutContent: UILabel!   // Created by, Libraries, Translators
     @IBOutlet weak var infoAppAboutOverview: UILabel!
     @IBOutlet weak var infoAppAboutVersion: UILabel!
     @IBOutlet weak var infoAppAboutWelcome: UILabel!
@@ -56,8 +55,17 @@ class InfoMenuAboutTableVC: UITableViewController {
         navigationItem.setLeftBarButton(barItem, animated: false)
         
         infoAppAboutAppName.text = NSLocalizedString("info_app_about_app_name", comment: "")
-        infoAppAboutCreatedBy.text = NSLocalizedString("info_app_about_created_by", comment: "")
-        infoAppAboutOssCredits.text = NSLocalizedString("info_app_about_oss_credits", comment: "")
+        
+        let aboutContentStr = """
+        \(NSLocalizedString("info_app_about_created_by", comment: ""))
+        
+        \(NSLocalizedString("info_app_about_oss_credits", comment: ""))
+        
+        \(NSLocalizedString("info_app_about_translators", comment: ""))
+        
+        """
+
+        infoAppAboutContent.text = aboutContentStr
         infoAppAboutOverview.text = NSLocalizedString("info_app_about_overview", comment: "")
         infoAppAboutVersion.text = NSLocalizedString("info_app_about_version", comment: "")
         infoAppAboutWelcome.text = NSLocalizedString("info_app_about_welcome", comment: "")
