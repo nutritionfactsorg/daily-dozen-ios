dab0a5a1d52af358f3b0c3ffd4e2ab7bab212833
marc-medley <marc-medley@users.noreply.github.com>
2022-11-17 12:30:52 -0800

    fix first launch screen (issue #65)

--- DailyDozen/DailyDozen/App/Controllers/FirstLaunchViewController.swift
+++ DailyDozen/DailyDozen/App/Controllers/FirstLaunchViewController.swift
@@ -27,12 +27,13 @@ class FirstLaunchViewController: UIViewController {
     
     @IBOutlet weak var settingHealthAloneLabel: UILabel!
     @IBOutlet weak var settingHealthWeightLabel: UILabel!
-    @IBOutlet weak var settingDozeOnlyBtn: UIButton!
-    @IBOutlet weak var settingDozeTweakBtn: UIButton!
-        
+    
+    @IBOutlet weak var settingDozeOnlyButton: UIButton!
+    @IBOutlet weak var settingDozeTweakButton: UIButton!
+    
     // Actions
     
-    @IBAction func dailyDozenOnly(_ sender: Any) {
+    @IBAction func dailyDozenOnlyBtnAction(_ sender: UIButton) {
         UserDefaults.standard.set(true, forKey: SettingsKeys.hasSeenFirstLaunch)
         UserDefaults.standard.set(false, forKey: SettingsKeys.show21TweaksPref)
         NotificationCenter.default.post(
@@ -41,7 +42,7 @@ class FirstLaunchViewController: UIViewController {
             userInfo: nil)
     }
     
-    @IBAction func dozenPlusTweaks(_ sender: Any) {
+    @IBAction func dozenPlusTweaksBtnAction(_ sender: UIButton) {
         // NYI: Insert Weight permisisons here
         UserDefaults.standard.set(true, forKey: SettingsKeys.hasSeenFirstLaunch)
         UserDefaults.standard.set(true, forKey: SettingsKeys.show21TweaksPref)
@@ -67,16 +68,28 @@ class FirstLaunchViewController: UIViewController {
 
         settingHealthAloneLabel.text = NSLocalizedString("setting_health_alone_txt", comment: "For Health Alone")
         settingHealthWeightLabel.text = NSLocalizedString("setting_health_weight_txt", comment: "For Health and Weight Loss")
-        settingDozeOnlyBtn.backgroundColor = ColorManager.style.mainMedium
-        settingDozeOnlyBtn.setTitleColor(UIColor.white, for: .normal)
-        settingDozeOnlyBtn.setTitle(
+                
+        settingDozeOnlyButton.backgroundColor = ColorManager.style.mainMedium
+        settingDozeOnlyButton.tintColor = ColorManager.style.mainMedium
+        settingDozeOnlyButton.setTitleColor(UIColor.white, for: .normal)
+        settingDozeOnlyButton.layer.cornerRadius = 10
+        settingDozeOnlyButton.layer.shadowRadius = 10
+        settingDozeOnlyButton.titleLabel?.textAlignment = .center
+        settingDozeOnlyButton.setTitle(
             NSLocalizedString("setting_doze_only_btn", comment: "Daily Dozen\nOnly"),
             for: .normal)
-        settingDozeTweakBtn.backgroundColor = ColorManager.style.mainMedium
-        settingDozeTweakBtn.setTitleColor(UIColor.white, for: .normal)
-        settingDozeTweakBtn.setTitle(
+        settingDozeOnlyButton.titleLabel?.lineBreakMode = NSLineBreakMode.byWordWrapping
+        
+        settingDozeTweakButton.backgroundColor = ColorManager.style.mainMedium
+        settingDozeTweakButton.tintColor = ColorManager.style.mainMedium
+        settingDozeTweakButton.setTitleColor(UIColor.white, for: .normal)
+        settingDozeTweakButton.layer.cornerRadius = 10
+        settingDozeTweakButton.layer.shadowRadius = 10
+        settingDozeTweakButton.titleLabel?.textAlignment = .center
+        settingDozeTweakButton.setTitle(
             NSLocalizedString("setting_doze_tweak_btn", comment: "Daily Dozen +\n21 Tweaks"),
             for: .normal)
+        settingDozeTweakButton.titleLabel?.lineBreakMode = NSLineBreakMode.byWordWrapping
     }
     
 }
