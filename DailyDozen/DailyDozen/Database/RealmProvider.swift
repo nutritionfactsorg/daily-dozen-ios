db563282ea0889bfe2aeff0513fb9f42c12adbd4
marc-medley <marc-medley@users.noreply.github.com>
2023-08-24 23:45:09 -0700

    Realm generic needs an [String: Any] array type.

--- DailyDozen/DailyDozen/Database/RealmProvider.swift
+++ DailyDozen/DailyDozen/Database/RealmProvider.swift
@@ -397,7 +397,7 @@ class RealmProvider {
             try realm.write {
                 realm.create(
                     RealmDataCountRecord.self,
-                    value: ["pid": pid, "count": count],
+                    value: ["pid": pid, "count": count] as [String : Any],
                     update: Realm.UpdatePolicy.modified)
                 updateStreak(count: count, date: date, countType: countType)
             }
@@ -416,7 +416,7 @@ class RealmProvider {
             try realm.write {
                 realm.create(
                     RealmDataWeightRecord.self,
-                    value: ["pid": pid, "kg": kg, "time": date.datestampHHmm],
+                    value: ["pid": pid, "kg": kg, "time": date.datestampHHmm] as [String : Any],
                     update: Realm.UpdatePolicy.all)
             }
         } catch {
