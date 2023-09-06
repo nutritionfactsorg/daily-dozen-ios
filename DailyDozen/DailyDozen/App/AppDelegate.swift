9c339037b359e7b7c66d8bf86e5c6f0729abb27a
marc-medley <marc-medley@users.noreply.github.com>
2023-09-06 14:44:43 -0700

    updates from main branch. iOS 15.1

--- DailyDozen/DailyDozen/App/AppDelegate.swift
+++ DailyDozen/DailyDozen/App/AppDelegate.swift
@@ -21,26 +21,37 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     var window: UIWindow?
     
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
         let logger = LogService.shared
         logger.logLevel = LogServiceLevel.off
         
         #if DEBUG
-        print("PRODUCT_BUNDLE_IDENTIFIER = \(Bundle.main.bundleIdentifier ?? "not found")")
+        let identifier = Bundle.main.bundleIdentifier ?? "not found"
+        print("PRODUCT_BUNDLE_IDENTIFIER = \(identifier)")
         
         printDeviceInfo()
         printLocaleInfo()
         
-        print(":DEBUG:WAYPOINT: AppDelegate didFinishLaunchingWithOptions\n\((URL.inDocuments().path))")
+        print("""
+        :DEBUG:WAYPOINT: AppDelegate didFinishLaunchingWithOptions
+        \((URL.inDocuments().path))
+        """)
         logger.logLevel = LogServiceLevel.verbose
         logger.useLogFile(nameToken: "dev") // logger.useLogFileDefault()
-        logger.debug("::::: DEBUG :::::\nAppDelegate didFinishLaunchingWithOptions DEBUG enabled")
+        logger.debug("""
+        ::::: DEBUG :::::
+        AppDelegate didFinishLaunchingWithOptions DEBUG enabled
+        """)
         
         DatabaseBuiltInTest.shared.runSuite()
         
         logger.debug(":::::::::::::::::\n")
         #endif
         
+        // =====  SIMULATOR SETUP  =====
         #if targetEnvironment(simulator)
         let bundle = Bundle(for: type(of: self))
         logger.debug("""
@@ -49,19 +60,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         App Documents (log files and exports) Directory:\n\(URL.inDocuments().path)\n
         App Library (database) Directory:\n\(URL.inLibrary().path)\n
         """)
-
-        /*
-        // Preliminary integrity checks.
-        let realmMngrOldCheck = RealmManagerLegacy(workingDirUrl: URL.inDocuments())
-        let realmDbOldCheck = realmMngrOldCheck.realmDb
-        // World Pasta Day: Oct 25, 1995
-        let date1995Pasta = Date(datestampKey: "19951025")!
-        // Add known content to legacy
-        let dozeCheck = realmDbOldCheck.getDozeLegacy(for: date1995Pasta)
-        realmDbOldCheck.saveStatesLegacy([true, false, true], id: dozeCheck.items[0].id) // Beans
-        realmDbOldCheck.saveStatesLegacy([false, true, false], id: dozeCheck.items[2].id) // Other Fruits
-        */
- 
         logger.debug(":::::::::::::::::::::::::::::::::\n")
         #endif
         
@@ -103,9 +101,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             Analytics.setAnalyticsCollectionEnabled(false)
             LogService.shared.info("AppDelegate setAnalyticsCollectionEnabled(false)")
         }
-        #if DEBUG
-        #else
-        #endif
         
         return true
     }
